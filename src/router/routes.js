f00e0dd1c4c0116f89cc1c1556047d16d7ca924a
xiaocaiyuan <3043211906@qq.com>
2023-05-24 15:01:46 +0800

    初始化项目结构

---
+++ src/router/routes.js
@@ -0,0 +1,9 @@
+import LoginView from '@/views/LoginView.vue'
+const routes = [
+	{
+		path: '/login',
+		component: LoginView,
+	},
+]
+
+export default routes
---
+++ src/views/LoginView.vue
@@ -0,0 +1,7 @@
+<style scoped lang="less"></style>
+
+<template>
+	<div class="login">login</div>
+</template>
+
+<script></script>
