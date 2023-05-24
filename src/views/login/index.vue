8a0c68f8f2369076147dc001bd5ebc3b49c665fd
xiaocaiyuan <3043211906@qq.com>
2023-05-24 22:59:31 +0800

    完成基本登录功能，处理错误响应

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,7 +14,8 @@
 	.login-btn-wrap {
 		padding: 53px 33px;
 		.login-btn {
-			background-color: #6db4fb;border: 0;
+			background-color: #6db4fb;
+			border: 0;
 		}
 	}
 }
@@ -26,10 +27,10 @@
 		<van-nav-bar class="page-nav-bar" title="登录" />
 		<!--登录表单-->
 		<van-form @submit="onSubmit">
-			<van-field type="text" name="手机号" placeholder="请输入手机号"
+			<van-field type="number" name="手机号" placeholder="请输入手机号" v-model="user.mobile"
 				><i slot="left-icon" class="toutiao toutiao-shouji"></i
 			></van-field>
-			<van-field type="password" name="验证码" placeholder="请输入验证码">
+			<van-field type="number" name="验证码" placeholder="请输入验证码" v-model="user.code">
 				<i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
 				<template #button>
 					<van-button round size="small" type="default" class="send-sms-btn"
@@ -47,7 +48,34 @@
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
 	name: 'LoginIndex',
+	data() {
+		return {
+			user: {
+				mobile: '',
+				code: '',
+			},
+		}
+	},
+	methods: {
+		// 表单提交
+		async onSubmit() {
+			const user = this.user // 表单数据
+			try {
+				const res = await login(user)
+				const data = res.data
+				console.log(data)
+			} catch (err) {
+				const status = err.response.status
+				if (status === 400) {
+					console.log('手机号或验证码错误')
+				} else {
+					console.log('登录失败，请稍后重试！')
+				}
+			}
+		},
+	},
 }
 </script>
